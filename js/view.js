2c11b6b46b71ad794b6d4048682798e5bfbff494
jaredwy <jared.wyles@gmail.com>
2010-09-28 20:29:34 +1000

    added a quick dialog

--- js/view.js
+++ js/view.js
@@ -1,9 +1,19 @@
-var addCal = function(title){
-    var toAdd = $('<li class="arrow">'+ title +'</li>').bind('click',function(e) { 
-        
-    });
-    $(".rounded").append(toAdd);
-};
-$(window).load(function(){
-    $.jQTouch();    
+$(document).ready(function(){
+    	$( "#bookingForm" ).dialog({
+					autoOpen: false,
+					height: 300,
+					width: 350,
+					modal: true,
+					buttons: {
+						Book: function() {
+								alert('hello');
+							},
+						Cancel: function() {
+							$( this ).dialog( "close" );
+						}
+					},
+					close: function() {
+					}
+		});
+       
 });
